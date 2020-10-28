c215891bce647a878d7791d6d7496b9df972357e
Geyuansu <957129604@qq.com>
2020-10-28 13:07:50 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<vector>
+#include"ParticleSwarmOptimization.h"
+#include"AntColonyOptimization.h"
+
+using namespace std;
+
+
+//main of ParticleSwarmOptimization
+//int main()
+//{
+//    init();
+//    PSO();
+//    system("pause");
+//    return 0;
+//}
+
+//main of AntColonyOptimization
+
